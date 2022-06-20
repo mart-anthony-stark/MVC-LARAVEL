ac0493e4386798417c106370faf3c162a5f10a96
mart-anthony-stark <martsalazar0@gmail.com>
2022-06-20 21:54:17 +0800

    Add: pizzas view

--- resources/views/pizzas.blade.php
+++ resources/views/pizzas.blade.php
@@ -1,8 +1,17 @@
 @extends('layouts.layout')
 
 @section('content')
-    <h1>Pizzas</h1>
-    @foreach ($pizzas as $pizza)
-        <h3>{{ $pizza }}</h3>
-    @endforeach
+    <div class="flex justify-center items-center flex-column">
+
+        <h1>Pizzas</h1>
+        @if ($name)
+            <h2>Name: {{ $name }}</h2>
+        @endif
+        @if ($age)
+            <h2>Age: {{ $age }}</h2>
+        @endif
+        @foreach ($pizzas as $pizza)
+            <h3>{{ $loop->index + 1 }} {{ $pizza['type'] }} - {{ $pizza['base'] }}</h3>
+        @endforeach
+    </div>
 @endsection
