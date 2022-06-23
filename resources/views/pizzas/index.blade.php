c8c04914b6e1c18679eb7e98ed72a57ec3073887
mart-anthony-stark <martsalazar0@gmail.com>
2022-06-23 20:09:10 +0800

    Add: session message

--- resources/views/pizzas/index.blade.php
+++ resources/views/pizzas/index.blade.php
@@ -2,10 +2,16 @@
 
 @section('content')
     <div class="flex justify-center items-center flex-column">
+        @if (session('msg'))
+            <p class="msg">{{ session('msg') }}</p>
+        @endif
 
         <h1>Pizzas</h1>
+        <a href="/pizzas/create">ADD NEW PIZZA</a>
         @foreach ($pizzas as $pizza)
-            <h3> {{ $pizza->type }} - {{ $pizza->base }} - {{ $pizza->name }} - Php {{ $pizza->price }}</h3>
+            <a href="/pizzas/{{ $pizza->id }}">
+                <h3> {{ $pizza->type }} - {{ $pizza->base }} - {{ $pizza->name }} - Php {{ $pizza->price }}</h3>
+            </a>
         @endforeach
     </div>
 @endsection
