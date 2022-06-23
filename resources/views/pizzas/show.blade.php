4f684626c945bb96081c7405707ce53c99c23208
mart-anthony-stark <martsalazar0@gmail.com>
2022-06-23 19:01:05 +0800

    Add: back link

--- resources/views/pizzas/show.blade.php
+++ resources/views/pizzas/show.blade.php
@@ -1,6 +1,7 @@
 @extends('layouts.layout')
 
 @section('content')
+    <a href="/pizzas">Back to all pizzas</a>
     <h1>{{ $pizza->name }}</h1>
     <p class="type">{{ $pizza->type }}</p>
     <p class="base">{{ $pizza->base }}</p>
