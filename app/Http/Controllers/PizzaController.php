3f0c84272d4dbe11bc8f15e60fdc4d7d25f32614
mart-anthony-stark <martsalazar0@gmail.com>
2022-06-23 16:05:01 +0800

    Add: Eloquent ORM methods

--- app/Http/Controllers/PizzaController.php
+++ app/Http/Controllers/PizzaController.php
@@ -9,6 +9,9 @@ class PizzaController extends Controller
 {
     public function index(){
         $pizzas = Pizza::all();
+        // $pizzas = Pizza::orderBy('name', 'asc    ')->get();
+        // $pizzas = Pizza::where('type', 'hawaiian')->get();
+        // $pizzas = Pizza::latest()->take(2)->get();
         return view('pizzas.index', ['pizzas'=>$pizzas]);
     }
 
