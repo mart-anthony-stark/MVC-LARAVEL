c2659622208a6a4a1066fcdcb6420c3db68ea80f
mart-anthony-stark <martsalazar0@gmail.com>
2022-06-25 19:42:46 +0800

    change: route name

--- resources/views/pizzas/index.blade.php
+++ resources/views/pizzas/index.blade.php
@@ -7,7 +7,7 @@
         @endif
 
         <h1>Pizzas</h1>
-        <a href="/pizzas/create">ADD NEW PIZZA</a>
+        <a href="{{ route('pizzas.create') }}">ADD NEW PIZZA</a>
         @foreach ($pizzas as $pizza)
             <a href="/pizzas/{{ $pizza->id }}">
                 <h3> {{ $pizza->type }} - {{ $pizza->base }} - {{ $pizza->name }} - Php {{ $pizza->price }}</h3>
